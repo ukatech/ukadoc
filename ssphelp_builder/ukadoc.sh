0255f6af3938068edd2fb299f7c8204da28234ae
ponapalt <ponapalt@40595f21-b55b-3940-adf6-1146fb0d9732>
2014-11-11 05:25:29 +0000

    シェルスクリプト追加

--- ssphelp_builder/ukadoc.sh
+++ ssphelp_builder/ukadoc.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-export LANG=ja_JP.UTF-8
-
-PATH="$PATH:/usr/local/bin"
-
-svn update /home/web/bugtraq/ukadoc-src
-
-cd /home/web/bugtraq/ukadoc-src/ssphelp_builder/
-
 sh node_modules/.bin/coffee html_content_extract.coffee ../ssphelp src
 node node_modules/indexsite/indexsite.js do
 node node_modules/nwordlinks/nwordlinks.js -d ./dist/ -w reference.html -s "#contents dl" -c "#contents" -x -h
@@ -16,6 +8,3 @@ cp -f dist/* ../../ukadoc/ssphelp/
 cp -f -R dist/lib ../../ukadoc/ssphelp/
 cp -f -R ../ssphelp/image ../../ukadoc/ssphelp/
 cp -f -R ../manual ../../ukadoc
-
-svn revert src
-svn revert dist
