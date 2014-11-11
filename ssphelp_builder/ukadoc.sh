77dad4c6eb67804e0bcaba880a6a5e93c3a52c3f
t_sato <t_sato@narazaka.net>
2014-11-11 17:38:20 +0000

    SSP HELP Builder: dist -> /ssphelp

--- ssphelp_builder/ukadoc.sh
+++ ssphelp_builder/ukadoc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 node node_modules/indexsite/indexsite.js do
-node node_modules/nwordlinks/nwordlinks.js -d ./dist/ -w reference.html -s "#contents dl" -c "#contents" -x -h
-node node_modules/all-site-search/ass-indexer.js -i search.html -o dist/assdb.json dist
+node node_modules/nwordlinks/nwordlinks.js -d ../ssphelp/ -w reference.html -s "#contents dl" -c "#contents" -x -h
+node node_modules/all-site-search/ass-indexer.js -i search.html -o ../ssphelp/assdb.json ../ssphelp
 
-cp -f dist/* ../../ukadoc/ssphelp/
-cp -f -R dist/lib ../../ukadoc/ssphelp/
-cp -f -R dist/image ../../ukadoc/ssphelp/
+cp -f ../ssphelp/* ../../ukadoc/ssphelp/
+cp -f -R ../ssphelp/lib ../../ukadoc/ssphelp/
+cp -f -R ../ssphelp/image ../../ukadoc/ssphelp/
 cp -f -R ../manual ../../ukadoc
