ec76c9498742260e56f584aa4b83a06f6a41b0dc
c6h2.oh.co2h <c6h2.oh.co2h@40595f21-b55b-3940-adf6-1146fb0d9732>
2014-12-19 14:36:18 +0000

    リダイレクト用JavaScriptを、urlがhttpsの場合用に修正

--- trunk/manual/ukadocred.js
+++ trunk/manual/ukadocred.js
@@ -1,2 +1,2 @@
 var href = location.href;
-if(/ukadoc\.googlecode\.com/.test(href)) location.replace(href.replace('ukadoc.googlecode.com/svn/trunk/manual/', 'ssp.shillest.net/ukadoc/manual/'));
+if(/ukadoc\.googlecode\.com/.test(href)) location.replace(href.replace(/https?:\/\/ukadoc\.googlecode\.com\/svn\/trunk\//, 'http://ssp.shillest.net/ukadoc/'));
