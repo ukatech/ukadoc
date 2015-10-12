d60a1d471bdf5f8bc6b1489217378b5599009cf6
gallnuts <c6h2_oh_3co2h@yahoo.co.jp>
2015-10-12 17:08:12 +0900

    google codeからshillestへのリダイレクト用スクリプトを無効化

--- manual/ukadocred.js
+++ manual/ukadocred.js
@@ -1,2 +1,5 @@
-var href = location.href;
-if(/ukadoc\.googlecode\.com/.test(href)) location.replace(href.replace(/https?:\/\/ukadoc\.googlecode\.com\/svn\/trunk\//, 'http://ssp.shillest.net/ukadoc/'));
+// googlecode上のファイルへ直接リンクがあった場合に、公開用サーバへのリダイレクトを行う
+// 2015/10/12　ファイル管理がGitHubに移転したことに伴って無効化
+
+//var href = location.href;
+//if(/ukadoc\.googlecode\.com/.test(href)) location.replace(href.replace(/https?:\/\/ukadoc\.googlecode\.com\/svn\/trunk\//, 'http://ssp.shillest.net/ukadoc/'));
