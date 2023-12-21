9ac8cc660cda3cd8f29ba6aef644faa84fa4b2f4
steve02081504 <steve02081504@EDEN.fukwold>
2023-12-21 18:19:56 +0800

    Update sakura_player.js

--- manual/sakura_player.js
+++ manual/sakura_player.js
@@ -15,18 +15,27 @@ async function init_content() {
 	// 为其增加一个按钮，点击后执行SakuraScript
 	sakuraScriptCodes.forEach(code => {
 		const button = createExecutionButton(code.textContent);
-		code.parentElement.insertBefore(button, code);
+		const parent = code.parentElement.parentElement;
+		// 若父元素的子元素除了h1外只有一个元素，那么将按钮插入到h1后面
+		if (parent.children.length === 2) {
+			parent.insertBefore(button, parent.children[1]);
+			//取消h1的换行
+			parent.children[0].style.display = "inline";
+		}
+		// 否则插入到代码块前面
+		else
+			code.parentElement.insertBefore(button, code);
 	});
 }
 
 function createExecutionButton(script) {
 	const button = document.createElement("button");
-	button.textContent = "执行";
+	button.textContent = "実行";
 	// 为按钮添加悬浮提示
-	button.title = "执行SakuraScript";
+	button.title = "SakuraScriptを実行";
 	button.addEventListener("click", () => {
 		jsstp.SEND({
-			Event: "OnUkaDocScriptExample",
+			Event: "OnUkadocScriptExample",
 			Reference0: script,
 			Script: script,
 			Option: "notranslate"
